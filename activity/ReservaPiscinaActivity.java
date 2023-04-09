92b2893326e4391db66a45b5138f65286a888847
MiguelAngelzz29 <miguelzz29@gmail.com>
2023-04-09 13:09:35 +0200

    android studio commit

---
+++ activity/ReservaPiscinaActivity.java
@@ -0,0 +1,16 @@
+package es.miguel.polideportivo.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import es.miguel.polideportivo.R;
+
+public class ReservaPiscinaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserva_piscina);
+    }
+}
